d5d47a3be36834b1507eee83438b3734011be168
Albahsanny11 <appsalbahsanny@gmail.com>
2025-05-30 04:07:07 +0300

    Update predict_stock.py

--- predict_stock.py
+++ predict_stock.py
@@ -3,7 +3,10 @@ try:
     from sklearn.model_selection import train_test_split
     import yfinance as yf
     import pandas as pd
+    import numpy as np
+    from sklearn.ensemble import RandomForestClassifier
     import joblib
+    import os
     from datetime import datetime, timedelta
 except ImportError as e:
     print(f"❌ Missing dependencies: {e}")
